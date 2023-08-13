17e91eaa1ed77be45f363118f8d487800a117014
mrityu4 <raimritunjai1@gmail.com>
2023-08-14 04:36:48 +0530

    add transaction

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,33 @@
-import { SignInButton,SignOutButton, useUser } from "@clerk/nextjs";
-import {api} from '../utils/trpc'
+import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
+import { api } from "../utils/trpc";
 
 export default function Home() {
-  const user=useUser();
-  const {data} = api.example.getAll.useQuery();
-
-
+  const user = useUser();
+  const { data } = api.example.getAll.useQuery();
+  const ctx = api.useContext();
+  const { mutate, isLoading: isPosting } = api.example.add.useMutation({
+    onSuccess: () => {
+      void ctx.example.getAll.invalidate();
+    },
+  });
 
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 `}
-    > 
+    >
       {!user.isSignedIn && <SignInButton />}
       {!!user.isSignedIn && <SignOutButton />}
+      <button
+        onClick={() =>
+          mutate({
+            amount: 1,
+            doneAt: "2020-01-01T00:00:00Z",
+            recipient: "abc",
+            isCredit: true,
+            desc:'test'
+          })
+        }
+      >Add</button>
     </main>
-  )
+  );
 }
